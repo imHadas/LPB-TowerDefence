1dbf4b2ac2c2423223a25d951a2b1c8036751c2f
Mózes László Máté <gbccm1@inf.elte.hu>
2022-03-18 17:50:09 +0100

    Table ctor changed to uint, changed table setup and view exit now has eventhandler

--- TowerDefence/TowerDefenceGame_LPB/Persistence/Table.cs
+++ TowerDefence/TowerDefenceGame_LPB/Persistence/Table.cs
@@ -22,14 +22,20 @@ namespace TowerDefenceGame_LPB.Persistence
             set { fields[c.x,c.y] = value; }
         }
 
-        public Table(int height, int width)
+        public Table(uint height, uint width)
         {
             fields = new Field[height, width];
         }
 
         public IEnumerator GetEnumerator()  // make it easier to iterate through all fields
         {
-            throw new System.NotImplementedException();
+            for (uint i = 0; i < fields.GetLength(0); i++)
+            {
+                for (uint j = 0; j < fields.GetLength(1); j++)
+                {
+                    yield return fields[i, j];
+                }
+            }
         }
     }
 }
