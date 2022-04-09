4f3cbd01c34f0a67d588656675fd7eaa8ab70d7e
Mózes László Máté <gbccm1@inf.elte.hu>
2022-04-09 14:55:17 +0200

    Complete rewrite of saving and refactored parts of Field, Placement and Player

--- TowerDefence/TowerDefenceGame_LPB/DataAccess/JsonDataAccess.cs
+++ TowerDefence/TowerDefenceGame_LPB/DataAccess/JsonDataAccess.cs
@@ -4,36 +4,279 @@ using TowerDefenceGame_LPB.Persistence;
 using System.IO;
 using System.Text.Json;  //use the .NET 6 serializer
 using System.Text.Json.Serialization;
+using System.Collections.Generic;
 
 namespace TowerDefenceGame_LPB.DataAccess
 {
+    internal class StringifiedUnit
+    {
+        public string Type { get; set; }
+        public uint Health { get; set; }
+
+        public StringifiedUnit(Unit unit)
+        {
+            Type = unit is TankUnit ? "TankUnit" : "BasicUnit";
+            Health = unit.Health;
+        }
+    }
+
+    internal class StringifiedPlacement
+    {
+        public string Type { get; set; }
+        public string Owner { get; set; }
+        public uint Level { get; set; }
+
+        public StringifiedPlacement(Placement placement)
+        {
+            Type = placement.GetType().Name;
+            Owner = placement.OwnerType.ToString();
+            if(placement is Tower) Level = ((Tower)placement).Level;
+            else if(placement is Terrain) Level = (uint)((Terrain)placement).Type;  //a bit hacky
+            else Level = 0;
+        }
+    }
+
+    internal class StringifiedField
+    {
+        public (uint, uint) Coords { get; set; }
+        public StringifiedPlacement? Placement { get; set; }
+        public (ICollection<StringifiedUnit> red, ICollection<StringifiedUnit> blue) Units { get; set; }
+
+        public StringifiedField(Field field)
+        {
+            Coords = field.Coords;
+            if (field.Placement != null)
+                Placement = new(field.Placement);
+            else 
+                Placement = null;
+            
+            List<StringifiedUnit> red = new();
+            List<StringifiedUnit> blue = new();
+            foreach(Unit unit in field.Units)
+            {
+                if(unit.Owner.Type == PlayerType.RED)
+                {
+                    red.Add(new(unit));
+                }
+                else
+                {
+                    blue.Add(new(unit));
+                }
+            }
+
+            Units = (red, blue);
+        }
+    }
+
+
+    /// <summary>
+    /// You cannot serialize multi-dimensional arrays, that's why
+    /// </summary>
+    internal class StringifiedTable
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public StringifiedField[] Fields { get; set; }
+
+        public StringifiedTable(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Fields = new StringifiedField[Width * Height];
+        }
+        public StringifiedField this[uint x, uint y]
+        {
+            get => Fields[IndexFromCoords(y, x)];
+            set => Fields[IndexFromCoords(y, x)] = value;
+        }
+
+        public StringifiedField this[int x, int y]
+        {
+            get => this[(uint)x, (uint)y];
+            set => this[(uint)x, (uint)y] = value;
+        }
+
+
+        public StringifiedField this[(int x, int y) coords]
+        {
+            get => this[coords.x, coords.y];
+            set => this[coords.x, coords.y] = value;
+        }
+
+        public StringifiedField this[(uint x, uint y) coords]
+        {
+            get => this[coords.x, coords.y];
+            set => this[coords.x, coords.y] = value;
+        }
+
+        private uint IndexFromCoords(uint x, uint y)
+        {
+            return (uint)(x * Width + y);
+        }
+    }
+
+    internal class StringifiedGSO
+    {
+        public StringifiedTable Table { get; set; }
+        public uint BPMoney { get; set; }
+        public uint RPMoney { get; set; }
+
+        public StringifiedGSO(Table table, Player blueplayer, Player redplayer)
+        {
+            Table = new (table.Size.x, table.Size.y);
+            for (uint i = 0; i < table.Size.x; i++)
+            {
+                for(uint j = 0; j < table.Size.y; j++)
+                {
+                    Table[i, j] = new StringifiedField(table[i, j]);
+                }
+            }
+            BPMoney = blueplayer.Money;
+            RPMoney = redplayer.Money;
+        }
+    }
+
     public class JsonDataAccess : IDataAccess<GameSaveObject>
     {
         private static readonly JsonSerializerOptions _options
         = new() 
         { 
-            ReferenceHandler = ReferenceHandler.Preserve,
+            //ReferenceHandler = ReferenceHandler.Preserve,
             WriteIndented = true,
         };
 
         public async Task<GameSaveObject> LoadAsync(string path)
         {
-            GameSaveObject? gameSaveObject;
-            using (FileStream fs = File.Create(path))
+            StringifiedGSO? sgso;
+
+            using (FileStream fs = File.OpenRead(path))
             {
-                gameSaveObject = (GameSaveObject?)await JsonSerializer.DeserializeAsync(fs, typeof(GameSaveObject), _options);
+                sgso = await JsonSerializer.DeserializeAsync<StringifiedGSO>(fs, _options);
             }
 
-            if (gameSaveObject == null) throw new Exception("Error reading file");
+            if (sgso == null) throw new Exception("Error reading file");
 
-            return gameSaveObject;
+            return ConstructGSO(sgso);
 
         }
+
+
+        /// <summary>
+        /// Constructs Field for output Table. Inserts Units and Placements into Players as side effect.
+        /// </summary>
+        /// <param name="sf">Stringified Field to be parsed</param>
+        /// <param name="bp">Reference to output blue Player</param>
+        /// <param name="rp">Reference to output red Player</param>
+        /// <returns>Parsed Field</returns>
+        private Field ConstructField(StringifiedField sf, Player bp, Player rp)
+        {
+            Field output = new Field(sf.Coords.Item1, sf.Coords.Item2);
+            foreach(StringifiedUnit unit in sf.Units.red)
+            {
+                Unit nu;
+                if (unit.Type == "TankUnit")
+                    nu = new TankUnit(rp);
+                else
+                    nu = new BasicUnit(rp);
+                rp.Units.Add(nu);
+                output.Units.Add(nu);
+            }
+
+            foreach (StringifiedUnit unit in sf.Units.blue)
+            {
+                Unit nu;
+                if (unit.Type == "TankUnit")
+                    nu = new TankUnit(bp);
+                else
+                    nu = new BasicUnit(bp);
+                bp.Units.Add(nu);
+                output.Units.Add(nu);
+            }
+
+            if(sf.Placement is not null)
+            {
+                Player? owner;
+                switch(sf.Placement.Owner)
+                {
+                    case "RED": owner = rp; break;
+                    case "BLUE": owner = bp; break;
+                    case "NEUTRAL": owner = null; break;
+                    default: throw new Exception($"Owner of placement couldn't be parsed\n{sf.Placement.Owner}");
+                }
+
+                if(owner is null)
+                {
+                    if (sf.Placement.Type != "Terrain") throw new Exception($"Placement of type {sf.Placement.Type} must have owner");
+                    output.Placement = new Terrain(sf.Coords.Item1, sf.Coords.Item2, (TerrainType)sf.Placement.Level);
+                }
+                else switch(sf.Placement.Type)
+                {
+                    case "Castle":
+                            if (owner.Castle != null) 
+                                throw new Exception($"{owner.Type} has multiple castles");
+                            owner.Castle = new(owner, sf.Coords.Item1, sf.Coords.Item2);
+                            output.Placement = owner.Castle;
+                        break;
+                    case "Barrack":
+                            try
+                            {
+                                output.Placement = new Barrack(owner, sf.Coords.Item1, sf.Coords.Item2);
+                                owner.AddBarrack((Barrack)output.Placement);
+                            }
+                            catch (ArgumentException)
+                            {
+                                throw new Exception($"{owner.Type} would has more than 2 barracks");
+                            }
+                        break;
+                    case "BasicTower":
+                            output.Placement = new BasicTower(owner, (sf.Coords.Item1, sf.Coords.Item2));
+                            owner.Towers.Add((Tower)output.Placement);
+                        break;
+                    case "SniperTower":
+                            output.Placement = new SniperTower(owner, (sf.Coords.Item1, sf.Coords.Item2));
+                            owner.Towers.Add((Tower)output.Placement);
+                        break;
+                    case "BomberTower":
+                            output.Placement = new BomberTower(owner, (sf.Coords.Item1, sf.Coords.Item2));
+                            owner.Towers.Add((Tower)output.Placement);
+                        break;
+                }
+            }
+            else output.Placement = null;
+
+
+            return output;
+        }
+
+        private GameSaveObject ConstructGSO(StringifiedGSO sgso)
+        {
+            Table table = new((uint)sgso.Table.Height, (uint)sgso.Table.Width);
+            Player bp = new(PlayerType.BLUE);
+            Player rp = new(PlayerType.RED);
+
+            for(uint i = 0; i < table.Size.x; ++i)
+            {
+                for(uint j = 0; j < table.Size.y; ++j)
+                {
+                    table[i,j] = ConstructField(sgso.Table[i, j], bp, rp);
+                }
+            }
+
+            return new GameSaveObject(table, bp, rp);
+        }
+
         public async Task SaveAsync(string path, GameSaveObject gameSaveObject)
         {
+            StringifiedGSO sgso = new(
+                gameSaveObject.Table,
+                gameSaveObject.BluePlayer,
+                gameSaveObject.RedPlayer
+                );
+
             using (FileStream fs = File.Create(path))
             {
-                await JsonSerializer.SerializeAsync(fs, gameSaveObject, _options);
+                await JsonSerializer.SerializeAsync(fs, sgso, _options);
             }
         }
     }
--- TowerDefence/TowerDefenceGame_LPB/Persistence/Player.cs
+++ TowerDefence/TowerDefenceGame_LPB/Persistence/Player.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TowerDefenceGame_LPB.Persistence
 {
@@ -15,21 +15,30 @@ namespace TowerDefenceGame_LPB.Persistence
         public PlayerType Type { get; private set; }
         public uint Money { get; set; }  // changed to unsigned
 
-        public Castle Castle { get; set; }
+        public Castle? Castle { get; set; }
 
         // all have been changed to sets, since order doesn't matter
         public ISet<Tower> Towers { get; set; }
         public ISet<Unit> Units { get; set; }
-        public ISet<Barrack> Barracks { get; set; }
+        public ISet<Barrack> Barracks { get; private set; }
 
-        public Player(PlayerType type, Castle castle, ICollection<Barrack> barracks)
+        public Player(PlayerType type, Castle? castle = null, ICollection<Barrack>? barracks = null)
         {
             Castle = castle;
             Type = type;
             Money = Constants.PLAYER_STARTING_MONEY;
             Towers = new HashSet<Tower>();
             Units = new HashSet<Unit>();
-            Barracks = barracks.ToImmutableHashSet();  // immutable since the barracks won't change mid-game
+            if(barracks != null)
+                Barracks = new HashSet<Barrack>(barracks);
+            else
+                Barracks = new HashSet<Barrack>();
+        }
+
+        public void AddBarrack(Barrack barrack)
+        {
+            if (Barracks.Count <= 2) throw new ArgumentException("Player can't have more than 2 barracks");
+            Barracks.Add(barrack);
         }
     }
 }
